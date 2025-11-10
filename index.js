dc0ed9706b53f0150fb47dbd23b5856c7aeec3e2
Ladunnitegbe <93840071+Ladunnitegbe@users.noreply.github.com>
2025-11-10 01:07:52 +0100

    Add basic calculator functions in index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const display = document.getElementById("maindisplay");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = " ";
+}
+
+function deleteDisplay(){
+    display.value = display.value.toString().slice(0,-1);
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    
+}
